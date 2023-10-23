b22ac9e187c5d79d6590a742eb26f36a36b4e8c4
norahAlbarrak <112809556+norahAlbarrak@users.noreply.github.com>
2023-10-23 09:41:43 +0300

    Update ui.R

--- ui.R
+++ ui.R
@@ -43,8 +43,8 @@ ui <- dashboardPage(
       ),
       menuItem("Generate Network", tabName = "generate_network", icon = icon("sitemap")),
       menuItem("Download", tabName = "download", icon = icon("download")),
-      menuItem("Log in", tabName = "log_in", icon = icon("sign-in")),
-      menuItem("Register", tabName = "register", icon = icon("user-plus"))
+      menuItem("Log in", tabName = "Log_in", icon = icon("sign-in")),
+      menuItem("Register", tabName = "Register", icon = icon("user-plus"))
     )
   ),
   
@@ -120,7 +120,7 @@ ui <- dashboardPage(
                 width = 20,
                 
                 fluidRow(
-                  splitLayout(cellWidths = c("100%"),plotOutput("de_plot")
+                  splitLayout(cellWidths = c("100%"),tableOutput("de_plot")
                              
                               
                   )
@@ -158,59 +158,85 @@ ui <- dashboardPage(
               )
       ),
       
+      # Tools tab
       tabItem(tabName = "tools",
               h2("Tools"),
               p("Choose a network generation tool:"),
-              uiOutput("network_tool_boxes")
-      ),
+              selectInput("tool_choice", label = "Choose Network Generation Tool:", 
+                          choices = list("SeqNet", "DIANE"), 
+                          selected = "SeqNet")),
       
+      # Generate Network tab
       tabItem(tabName = "generate_network",
               h2("Generate Network"),
-              p("Network generation content goes here."),
-              fluidRow(
-                column(6, uiOutput("diane_network_box")),
-                column(6, uiOutput("seqnet_network_box"))
-              ),
-              actionButton("generate_button", "Generate Network"),
               
-              conditionalPanel(
-                "input.tool_choice.includes('seqnet')",
-                actionButton("get_sigma_button", "Get Sigma"),  # Add the Get Sigma button
-                actionButton("change_color", "Change Color")  # Add the Change Color button here
+              
+              actionButton("network_button", label = "Generate Network", 
+                           icon = icon("sitemap"), 
+                           style = "background-color: purple; color: white")
+              
+              ,
+              box(
+                
+                title = "Gene Network",
+                status = "primary",
+                solidHeader = TRUE,
+                width = 20,
+                
+                fluidRow(
+                  splitLayout(cellWidths = c("50%", "50%"),uiOutput("network_plot"),
+                              uiOutput("network_plot_2")
+                              
+                  )
+                  
+                )
               )
-              
-              
-              
-              
       ),
       
-      tabItem(tabName = "log_in",
+      
+      tabItem(tabName = "Log_in",  ##changed name here
               h2("Log in"),
-              div(
-                id = "login_button",
-                textInput("username", "Username"),
-                passwordInput("password", "Password"),
-                br(),
-                actionButton("login", "Log In")
-              )
+              
+              
+              div(id = "login_button", 
+                  
+                  textInput("username", "Username"),
+                  passwordInput("password", "Password"),
+                  br(),
+                  actionButton("login", "Log In") )
+              
       ),
       
-      tabItem(tabName = "register",
+      
+      
+      
+      
+      
+      
+      tabItem(tabName = "Register",
               h2("Register an Account"),
-              div(
-                id = "register_button",
-                tags$p("Don't have an account? Register below."), 
-                textInput("new_username", "New Username"),
-                passwordInput("new_password", "New Password"),
-                passwordInput("confirm_password", "Confirm Password"),
-                actionButton("register", "Register")
-              )
-      )
-    )
-  )
-)
-
-# ... (Previous code)
-
-
-
+              
+              div(id = "register_button",
+                  tags$p("Don't have an account? Register below."), 
+                  tags$p("Make sure your username is 8 characters long, your password is 8 characters long at least, has 2 special characters at least, and 1 capital letter at least.", style="color:#FF0000"),
+                  
+                  textInput("new_username", "New Username"),
+                  passwordInput("new_password", "New Password"),
+                  passwordInput("confirm_password", "Confirm Password"),
+                  actionButton("register", "Register")
+              )  
+              
+              
+      ))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  ))
